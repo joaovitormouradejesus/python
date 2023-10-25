dd8e2577e32505a3bdfddcea07622e9ab01e6b6c
João Vitor <joaov.mj120@gmail.com>
2023-10-25 17:09:40 -0300

    Create main.py

---
+++ exercicios (ignora)/ex002 (váriaveis, INPUT e conversão )/main.py
@@ -0,0 +1,14 @@
+nome = input("Digite seu nome:")
+#A primeira coisa que for escrito e logo em seguida colocar um "=" séra uma váriavel.
+#A função Input serve para a pessoa definir algo, e logo em seguida dentro das "" vai aparecer a pergunta a ser respondida.
+idade = int (input("Digite sua idade:"))
+#O int representa números inteiros e serve para converter esse valor para um número inteiro.
+peso = float (input("Digite seu peso:"))
+#O float representa Números décimais e serve para converter esse valor para um número décimal
+# .
+
+print(nome)
+#O que tiver dentro dos () sem as "" vai ser mostrado o valor da váriavel.
+print(type (idade))
+#O comando type sevre para mostrar a classe/tipo do valor da váriavel.
+print(type (peso))
